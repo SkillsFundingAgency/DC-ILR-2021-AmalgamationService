68f833df44ac93b1914a87891f38710614cbfa18
Ravendra mishra <mishra.ravendra@gmail.com>
2019-06-12 11:45:20 +0100

    Addressing review comments

--- src/ESFA.DC.ILR.AmalgamationService.Interfaces/Enum/Severity.cs
+++ src/ESFA.DC.ILR.AmalgamationService.Interfaces/Enum/Severity.cs
@@ -7,11 +7,6 @@ namespace ESFA.DC.ILR.AmalgamationService.Interfaces.Enum
     public enum Severity
     {
         Error,
-        Warning,
-
-        /// <summary>
-        /// File is determined to be unreadable, and not able to validated.
-        /// </summary>
-        Fail
+        Warning
     }
 }
