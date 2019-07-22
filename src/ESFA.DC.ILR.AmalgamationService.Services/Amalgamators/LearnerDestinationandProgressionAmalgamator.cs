a5ecd5d1b70352d044197da5dc3b14d65aa80b8c
Ravendra mishra <mishra.ravendra@gmail.com>
2019-07-22 13:49:08 +0000

    Learner HE related amalgamators

---
+++ src/ESFA.DC.ILR.AmalgamationService.Services/Amalgamators/LearnerDestinationandProgressionAmalgamator.cs
@@ -0,0 +1,30 @@
+﻿using ESFA.DC.ILR.AmalgamationService.Interfaces;
+using ESFA.DC.ILR.AmalgamationService.Interfaces.Enum;
+using ESFA.DC.ILR.AmalgamationService.Services.Amalgamators.Abstract;
+using ESFA.DC.ILR.Model.Loose.ReadWrite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ESFA.DC.ILR.AmalgamationService.Services.Amalgamators
+{
+    public class LearnerDestinationandProgressionAmalgamator : AbstractAmalgamator<MessageLearnerDestinationandProgression>, IAmalgamator<MessageLearnerDestinationandProgression>
+    {
+        private IRule<long?> _standardRuleLong;
+
+        public LearnerDestinationandProgressionAmalgamator(IRuleProvider ruleProvider, IAmalgamationErrorHandler amalgamationErrorHandler)
+            : base(Entity.LearnerDestinationandProgression, (x) => x.LearnRefNumber.ToString(), amalgamationErrorHandler)
+        {
+            _standardRuleLong = ruleProvider.BuildStandardRule<long?>();
+        }
+
+        public MessageLearnerDestinationandProgression Amalgamate(IEnumerable<MessageLearnerDestinationandProgression> models)
+        {
+            var messageLearnerDestinationandProgression = new MessageLearnerDestinationandProgression();
+
+            ApplyRule(s => s.ULNNullable, _standardRuleLong.Definition, models, messageLearnerDestinationandProgression);
+
+            return messageLearnerDestinationandProgression;
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.AmalgamationService.Services/Amalgamators/LearnerHEFinancialSupportAmalgamator.cs
@@ -0,0 +1,31 @@
+﻿using ESFA.DC.ILR.AmalgamationService.Interfaces;
+using ESFA.DC.ILR.AmalgamationService.Interfaces.Enum;
+using ESFA.DC.ILR.AmalgamationService.Services.Amalgamators.Abstract;
+using ESFA.DC.ILR.Model.Loose.ReadWrite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ESFA.DC.ILR.AmalgamationService.Services.Amalgamators
+{
+    public class LearnerHEFinancialSupportAmalgamator : AbstractAmalgamator<MessageLearnerLearnerHELearnerHEFinancialSupport>, IAmalgamator<MessageLearnerLearnerHELearnerHEFinancialSupport>
+    {
+        private IRule<long?> _standardRuleLong;
+
+        public LearnerHEFinancialSupportAmalgamator(IRuleProvider ruleProvider, IAmalgamationErrorHandler amalgamationErrorHandler)
+            : base(Entity.LearnerHEFinancialSupport, (x) => x.FINTYPE.ToString(), amalgamationErrorHandler)
+        {
+            _standardRuleLong = ruleProvider.BuildStandardRule<long?>();
+        }
+
+        public MessageLearnerLearnerHELearnerHEFinancialSupport Amalgamate(IEnumerable<MessageLearnerLearnerHELearnerHEFinancialSupport> models)
+        {
+            var messageLearnerLearnerHELearnerHEFinancialSupport = new MessageLearnerLearnerHELearnerHEFinancialSupport();
+
+            ApplyRule(s => s.FINTYPENullable, _standardRuleLong.Definition, models, messageLearnerLearnerHELearnerHEFinancialSupport);
+            ApplyRule(s => s.FINAMOUNTNullable, _standardRuleLong.Definition, models, messageLearnerLearnerHELearnerHEFinancialSupport);
+
+            return messageLearnerLearnerHELearnerHEFinancialSupport;
+        }
+    }
+}
