751b2d135e01cdfd592d83d50f7a765f6df8441f
Ravendra mishra <mishra.ravendra@gmail.com>
2019-08-05 10:29:34 +0000

    As per spec FilePreparationDate should be latest FilePreparationDate among input ILR files

--- src/ESFA.DC.ILR.AmalgamationService.Services/Amalgamators/HeaderCollectionDetailsAmalgamator.cs
+++ src/ESFA.DC.ILR.AmalgamationService.Services/Amalgamators/HeaderCollectionDetailsAmalgamator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using ESFA.DC.DateTimeProvider.Interface;
 using ESFA.DC.ILR.AmalgamationService.Interfaces;
@@ -12,21 +13,14 @@ namespace ESFA.DC.ILR.AmalgamationService.Services.Amalgamators
 {
     public class HeaderCollectionDetailsAmalgamator : AbstractAmalgamator<MessageHeaderCollectionDetails>, IAmalgamator<MessageHeaderCollectionDetails>
     {
-        private readonly IDateTimeProvider _dateTimeProvider;
-        private IRule<string> _standardRuleString;
-        private IRule<DateTime> _standardRuleDateTime;
-
-        public HeaderCollectionDetailsAmalgamator(IDateTimeProvider dateTimeProvider, IRuleProvider ruleProvider, IAmalgamationErrorHandler amalgamationErrorHandler)
+        public HeaderCollectionDetailsAmalgamator(IAmalgamationErrorHandler amalgamationErrorHandler)
             : base(Entity.CollectionDetails, (x) => null, amalgamationErrorHandler)
         {
-            _dateTimeProvider = dateTimeProvider;
-            _standardRuleString = ruleProvider.BuildStandardRule<string>();
-            _standardRuleDateTime = ruleProvider.BuildStandardRule<DateTime>();
         }
 
         public MessageHeaderCollectionDetails Amalgamate(IEnumerable<MessageHeaderCollectionDetails> models)
         {
-            return new MessageHeaderCollectionDetails() { FilePreparationDate = _dateTimeProvider.GetNowUtc() };
+            return new MessageHeaderCollectionDetails() { FilePreparationDate = models.Max(x => x.FilePreparationDate) };
         }
     }
 }
