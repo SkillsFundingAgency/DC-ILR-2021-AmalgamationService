793361bea74b52aec9ff3fab82a50bb0d1951e28
Ravendra mishra <mishra.ravendra@gmail.com>
2019-08-08 13:49:23 +0000

    Validation error reort

---
+++ src/ESFA.DC.ILR.AmalgamationService.Services/Mapper/ValidationErrorMapper.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration;
+using ESFA.DC.ILR.AmalgamationService.Interfaces;
+
+namespace ESFA.DC.ILR.AmalgamationService.Services.Mapper
+{
+    public sealed class ValidationErrorMapper : ClassMap<IValidationError>
+    {
+        public ValidationErrorMapper()
+        {
+            Map(m => m.XmlSeverity).Name("Severity");
+            Map(m => m.Message).Name("Message");
+            Map(m => m.LineNumber).Name("LineNumber");
+            Map(m => m.LinePosition).Name("LinePosition");
+            Map(m => m.XMLFileName).Name("FileName");
+        }
+    }
+}
